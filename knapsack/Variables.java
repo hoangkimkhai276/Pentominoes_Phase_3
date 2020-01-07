5c8fa512d9187805eb8c05eb28ccc8c8174058e2
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-01-08 00:21:26 +0100

    javadoc complete

--- knapsack/Variables.java
+++ knapsack/Variables.java
@@ -1,9 +1,15 @@
 package knapsack;
 
+/**
+ * interface containing common variables so that they can be altered from a single location
+ */
 public interface Variables {
 
+	/**default length (x-direction) of the knapsack in 0.5 meters */
 	public static final int DEFAULT_LENGTH 	= 33;	// 16.5 meter
+	/**default width (y-direction) of the knapsack in 0.5 meters */
 	public static final int DEFAULT_WIDTH 	= 5;	// 2.5  meter
+	/**default height (z-direction) of the knapsack in 0.5 meters */
 	public static final int DEFAULT_HEIGHT 	= 8;	// 4.0  meter
 	
 }
