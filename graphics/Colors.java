437c347b6ec00348c24a123348a848c357abbe0a
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-01-07 22:54:30 +0100

    Update Colors.java
    javadoccing complete

--- graphics/Colors.java
+++ graphics/Colors.java
@@ -10,10 +10,29 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ *
+ * This class provides methods for generating colors for use in the visual representation of the knapsack problem.
+ *
+ * @author anema
+ */
 public final class Colors {
-	
-	public static double minimal_color_distance = 80d;
-	
+
+	/** The minimal distance in bits two colors need to be from each other
+	 *  in order to be considered as two seperate colors.<br>This is used when generating multiple random colors at once
+	 *  and makes it so that the generated colors differ more from each other than normal randomly generated colors would be.
+	 *  <br><br>this value should be at least around 10 and at most around 500 (the theoretically most distant two colors can be is
+	 *  765: black (0,0,0) to white (255,255,255))<br><br>By default, {@code minimal_color_distance} equals {@code 80}*/
+	public static int minimal_color_distance = 80;
+
+	/**
+	 * Generates a random color based on the {@code minimal_color_distance} as step_size per R, G or B value to gradually move towards the specified
+	 * minimal brightness. The color also gets the specified transparency after generating the RGB values.
+	 * @param min_brightness the minimal brightness a generated color should be, with the brightness of a color being calculated as the
+	 *   sum of all RGB integer values divided by {@code (255x2)}, thereby giving a maximum brightness of {@code 765/520 = 1.5f}
+	 * @param transparency is equal to {@code 1.0f} minus the {@code alpha} value of the color to be generated
+	 * @return a random color generated using the specified criteria
+	 */
 	public static Color randomColor(float min_brightness, float transparency) {
 		Color result = new Color(0,0,0);
 		int desired_brightness = (int)(255 * 2 * min_brightness);
@@ -21,9 +40,9 @@ public final class Colors {
 		while (total_brightness < desired_brightness) {
 			double choice = Math.random() * 3;
 			int red = result.getRed(), green = result.getGreen(), blue = result.getBlue();
-			if (choice > 2) 	 red   = result.getRed() + (int)(Math.random() * minimal_color_distance);
-			else if (choice > 1) green = result.getGreen() + (int)(Math.random() * minimal_color_distance);
-			else 				 blue  = result.getBlue() + (int)(Math.random() * minimal_color_distance);
+			if (choice > 2) 	 red   = result.getRed() + (int)(Math.random() * minimal_color_distance)+1;
+			else if (choice > 1) green = result.getGreen() + (int)(Math.random() * minimal_color_distance)+1;
+			else 				 blue  = result.getBlue() + (int)(Math.random() * minimal_color_distance)+1;
 			if (red > 255) red = 255;
 			if (green > 255) green = 255;
 			if (blue > 255) blue = 255;
@@ -31,11 +50,32 @@ public final class Colors {
 			total_brightness = result.getRed() + result.getGreen() + result.getBlue();
 		} return new Color(result.getRed(), result.getGreen(), result.getBlue(), (int)(255 - 255*transparency));
 	}
-	
+
+	/**
+	 * Generates a certain amount of random colors using the {@code Colors.randomColor} method while keeping to
+	 *  the {@code minimal_color_distance} between each color.
+	 * @param amount amount of colors to be generated; could generate less if no color can be found that is the right
+	 *  distance away from the other generated colors.
+	 * @param brightness minimal brightness value for the colors generated as specified by the {@code Colors.randomColor} method
+	 * @param transparency the transparency value given to the generated colors as specified by the {@code Colors.randomColor} method
+	 * @return An array of colors, generated randomly using the specified criteria (length of the array is equal to the amount of colors
+	 * generated and thus not always equal to {@code amount})
+	 */
 	public static Color[] getRandomColors(int amount, float brightness, float transparency) {
 		return getRandomColors(amount, brightness, transparency, false);
 	}
-	
+
+	/**
+	 * Generates a certain amount of random colors using the {@code Colors.randomColor} method while keeping to
+	 *  the {@code minimal_color_distance} between each color.
+	 * @param amount amount of colors to be generated; could generate less if no color can be found that is the right
+	 *  distance away from the other generated colors.
+	 * @param brightness minimal brightness value for the colors generated as specified by the {@code Colors.randomColor} method
+	 * @param transparency the transparency value given to the generated colors as specified by the {@code Colors.randomColor} method
+	 * @param sorted specifies whether the resulting array of colors should be sorted or not
+	 * @return An array of colors, generated randomly using the specified criteria (length of the array is equal to the amount of colors
+	 * generated and thus not always equal to {@code amount})
+	 */
 	public static Color[] getRandomColors(int amount, float brightness, float transparency, boolean sorted) {
 		class CompColor {
 			public Color color;
@@ -72,15 +112,19 @@ public final class Colors {
 		for (int i=0; i < colors.size(); i++) result[i] = colors.get(i).color;
 		return result;
 	}
-	
+
+	/**
+	 * Showcases a little GUI that shows randomly generated colors in a grid pattern<br>
+	 * Clicking with the mouse re-generates the colors.
+	 * @param args not used
+	 */
 	public static void main(String[] args) {
-		
 		int count = 20;
 		float brightness = 0.8f;
 		float transparency = 0f;
 		boolean sorted = false;
 		minimal_color_distance = 80;
-		
+
 		Color[] colors = getRandomColors(count, brightness, transparency, sorted);
 		JFrame frame = new JFrame("colors!!!");
 		frame.setSize(500,500);
@@ -108,5 +152,5 @@ public final class Colors {
 			}
 		});
 	}
-	
+
 }
