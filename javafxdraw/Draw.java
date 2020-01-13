529606856d85321e0df2202ba86a77ef00543df9
hoangkimkhai276 <56165647+hoangkimkhai276@users.noreply.github.com>
2020-01-13 13:16:30 +0100

    Methods

--- javafxdraw/Draw.java
+++ javafxdraw/Draw.java
@@ -1,18 +1,24 @@
 package javafxdraw;
 
+import com.sun.deploy.uitoolkit.DragContext;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
+import javafxstuff.Point3D;
+import knapsack.Knapsack;
 import knapsack.parcel.Parcel;
 import knapsack.parcel.ParcelCore;
 import knapsack.parcel.Parcels;
@@ -36,90 +42,51 @@ public class Draw extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        SmartGroup group = new SmartGroup();
+//
+        run(primaryStage);
+
+    }
+    public void run(Stage primaryStage){
         Camera camera = new PerspectiveCamera();
-        Group c_group = new Group();
-        group.translateXProperty().set(100);
-        group.translateYProperty().set(100);
-        Parcel P = Parcels.P;
-        Box[] boxes = P.toBoxes(50);
-        for (int i = 0; i < boxes.length; i++) {
-            System.out.println(boxes[i].getTranslateX());
-            group.getChildren().add(boxes[i]);
-
-        }
-
-        SmartGroup t_group = new SmartGroup();
-        Parcel T = Parcels.T;
-        Box[] t_boxes = T.toBoxes(50);
-        for(int i = 0; i < t_boxes.length; i++){
-            t_group.getChildren().add(t_boxes[i]);
-        }
+        SmartGroup mainGroup = new SmartGroup();
+        SmartGroup p_group = toGroup(Parcels.P);
+        SmartGroup t_group = toGroup(Parcels.T);
+        SmartGroup l_group = toGroup(Parcels.L);
+        p_group.translateXProperty().set(200);
+        p_group.translateYProperty().set(200);
         t_group.translateXProperty().set(400);
-        t_group.translateYProperty().set(200);
+        t_group.translateYProperty().set(400);
+        l_group.translateXProperty().set(600);
+        l_group.translateYProperty().set(300);
 
-        c_group.getChildren().addAll(group,t_group);
-        Scene scene =new Scene(c_group,1000,500,true);
+        SmartGroup[] smartGroups = new SmartGroup[2];
+        smartGroups[0] = p_group;
+        smartGroups[1] = t_group;
+        mainGroup.getChildren().addAll(p_group,t_group,l_group);
+
+        Scene scene =new Scene(mainGroup,1000,500,true);
         scene.setFill(Color.SILVER);
         scene.setCamera(camera);
-//        Box box = new Box(100,100,100);
-//        group.getChildren().add(box);
-//        Scene scene = new Scene(group, 1400, 600);
-
-
-        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
-                    @Override
-                    public void handle(KeyEvent t) {
-                        switch (t.getCode()){
-                            case KP_UP:
-                                System.out.println(group.getTranslateZ()+" ");
-                                group.translateZProperty().set((group.getTranslateZ()+10));
-                                break;
-                            case KP_DOWN:
-                                group.translateZProperty().set(group.getTranslateZ()-10);
-                                break;
-                            case Q:
-                                group.rotateByX(-90 );
-                                break;
-                            case E:
-                                group.rotateByX(90);
-                                break;
-                            case W:
-                                group.rotateByZ(90);
-                                break;
-                            case S:
-                                group.rotateByZ(-90);
-                                break;
-                            case A:
-                                group.rotateByY(90);
-                                break;
-                            case D:
-                                group.rotateByY(-90);
-                                break;
-                            case NUMPAD4:
-                                System.out.println("Moving left");
-                                group.translateXProperty().set((group.getTranslateX()-10));
-                                break;
-                            case NUMPAD6:
-                                System.out.println("Moving right");
-                                group.translateXProperty().set((group.getTranslateX())+10);
-                                break;
-
-                        }
-                    }
-                }
-        );
-
+        System.out.println(mainGroup.getLayoutX()+  " dlksahdksa");
+        System.out.println(mainGroup.getLayoutY() + "dsadsa");
+        addEventHandler(primaryStage,p_group);
 
         primaryStage.setTitle("Khai");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
     class SmartGroup extends Group{
+        public SmartGroup(){
+
+        }
+        public SmartGroup(Parcel p, double scale){
+            Point3D origin = p.getOrigin();
+
+        }
+        Point3D origin;
         Rotate r;
         Transform t = new Rotate();
         void rotateByX(int angle){
@@ -140,9 +107,61 @@ public class Draw extends Application {
             this.getTransforms().clear();
             this.getTransforms().addAll(t);
         }
-        void moveX(int dis){
 
-        }
+
 
     }
+    private void addEventHandler(Stage primaryStage, SmartGroup group){
+        int angle = 90;
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent t) {
+                        switch (t.getCode()){
+                            case Q:
+                                group.rotateByX(-angle );
+                                break;
+                            case E:
+                                group.rotateByX(angle);
+                                break;
+                            case W:
+                                group.rotateByZ(angle);
+                                break;
+                            case S:
+                                group.rotateByZ(-angle);
+                                break;
+                            case A:
+                                group.rotateByY(angle);
+                                break;
+                            case D:
+                                group.rotateByY(-angle);
+                                break;
+                            case NUMPAD4:
+                                group.translateXProperty().set((group.getTranslateX()-10));
+                                System.out.println("Moving left" + group.translateXProperty().get());
+                                break;
+                            case NUMPAD6:
+                                group.translateXProperty().set((group.getTranslateX())+10);
+                                System.out.println("Moving right" + group.translateXProperty().get());
+                                break;
+
+                        }
+                    }
+                }
+        );
+    }
+    private SmartGroup toGroup(Parcel parcel){
+        SmartGroup parcelGroup = new SmartGroup();
+        Box[] boxes = parcel.toBoxes(50);
+        Point3D origin = parcel.getOrigin();
+        for (int i = 0; i < boxes.length; i++) {
+            System.out.println(boxes[i].getTranslateX());
+            parcelGroup.getChildren().add(boxes[i]);
+        }
+        return parcelGroup;
+    }
+
+
+
+
+
 }
