b65e219e145265adfbcaea1c065fbe4f23c72276
hoangkimkhai276 <56165647+hoangkimkhai276@users.noreply.github.com>
2020-01-17 11:38:39 +0100

    'mouseadded'

--- javafxdraw/Draw.java
+++ javafxdraw/Draw.java
@@ -1,20 +1,34 @@
 package javafxdraw;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import knapsack.KnapsackGroup;
 import knapsack.parcel.ParcelGroup;
 import knapsack.parcel.Parcels;
 
 public class Draw extends Application {
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 800;
+    private static final int angle = 5;
+    private double anchorX, anchorY;
+    private double anchorAngleX = 0;
+    private double anchorAngleY = 0;
+    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
+    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
+
     /**
      * The main entry point for all JavaFX applications.
      * The start method is called after the init method has returned,
@@ -33,89 +47,105 @@ public class Draw extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 //
-        Group root  = new Group();
-        KnapsackGroup knapsackGroup = KnapsackGroup.example;
-        Sphere sphere = new Sphere(25);
-        sphere.setTranslateX(0);
-        sphere.setTranslateY(0);
-        PerspectiveCamera camera = new PerspectiveCamera();
-        camera.setTranslateZ(-500);
-        camera.setTranslateY(-200);
+        draw(primaryStage);
 
-        root.getChildren().addAll(knapsackGroup,sphere);
-        Scene scene = new Scene(root, 1000,800,true);
+    }
+
+    private void draw(Stage primaryStage) {
+        Group root = new Group();
+        KnapsackGroup knapsackGroup = KnapsackGroup.example;
+        PerspectiveCamera camera = new PerspectiveCamera();
+
+        camera.setTranslateZ(-500);
+        root.getChildren().addAll(knapsackGroup);
+
+        knapsackGroup.setTranslateX(WIDTH/2-100);
+        knapsackGroup.setTranslateY(HEIGHT/2);
+        Scene scene = new Scene(root, WIDTH, HEIGHT, true);
         scene.setCamera(camera);
+
+        initMouseControl(knapsackGroup, scene);
         addEventHandler(primaryStage, knapsackGroup);
+
+        primaryStage.setTitle("Parcelminoes");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
 
+    private void initMouseControl(KnapsackGroup knapsackGroup, Scene scene) {
+        Rotate xRotate;
+        Rotate yRotate;
+        knapsackGroup.getTransforms().addAll(
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+
+        );
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+        scene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+        scene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY + (anchorX - event.getSceneX()));
+        });
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @SuppressWarnings("unused")
-	private void run(Stage primaryStage){
-        ParcelGroup parcelGroup = new ParcelGroup(Parcels.P,50);
 
-        PerspectiveCamera camera = new PerspectiveCamera();
-//        addEventHandler(primaryStage,parcelGroup);
-        Group mainGroup = new Group();
-        Line line = new Line(200,0,200,800);
-        mainGroup.getChildren().addAll(line,parcelGroup);
-        Scene scene = new Scene(mainGroup,1000,800,true);
-        scene.setFill(Color.SILVER);
-        scene.setCamera(camera);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    private void addEventHandler(Stage primaryStage, KnapsackGroup group){
-        int angle = 5;
+    private void addEventHandler(Stage primaryStage, KnapsackGroup group) {
+
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
                     @Override
                     public void handle(KeyEvent t) {
-                        switch (t.getCode()){
+                        switch (t.getCode()) {
                             case Q:
-//                                System.out.println(group.getOrigin().getX() + ", " +group.getOrigin().getY() + ", " +group.getOrigin().getZ());
-                                group.rotateByX(-angle );
+                                group.rotateByX(-angle);
                                 break;
                             case E:
                                 group.rotateByX(angle);
                                 break;
                             case W:
-//                                System.out.println(group.getOrigin().getX() + ", " +group.getOrigin().getY() + ", " +group.getOrigin().getZ());
                                 group.rotateByZ(angle);
                                 break;
                             case S:
                                 group.rotateByZ(-angle);
                                 break;
                             case A:
-//                                System.out.println(group.getOrigin().getX() + ", " +group.getOrigin().getY() + ", " +group.getOrigin().getZ());
                                 group.rotateByY(angle);
                                 break;
                             case D:
                                 group.rotateByY(-angle);
                                 break;
                             case NUMPAD4:
-                                group.translateXProperty().set((group.getTranslateX()-10));
+                                group.translateXProperty().set((group.getTranslateX() - 10));
                                 System.out.println("Moving left" + group.translateXProperty().get());
                                 break;
                             case NUMPAD6:
-                                group.translateXProperty().set((group.getTranslateX())+10);
+                                group.translateXProperty().set((group.getTranslateX()) + 10);
                                 System.out.println("Moving right" + group.translateXProperty().get());
                                 break;
-						default: break;
+                            case NUMPAD8:
+                                group.translateZProperty().set((group.getTranslateZ()) + 10);
+                                System.out.println("Moving forward" + group.translateXProperty().get());
+                                break;
+                            case NUMPAD2:
+                                group.translateZProperty().set((group.getTranslateZ()) - 10);
+                                System.out.println("Moving backward" + group.translateXProperty().get());
+                                break;
+                            default:
+                                break;
                         }
                     }
                 }
         );
-        primaryStage.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println(event.getX() + ", " + event.getY() + ", " + event.getZ());
-            }
-        });
+
     }
 
 
